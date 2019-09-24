43e7b11ec72f20fc6e2b1f44b6518734c92f97ac
Ignacio Acevedo <ig.acevedob@alumnos.duoc.cl>
2019-09-24 10:38:07 -0300

    ciclos

---
+++ ejemplo_ciclos.py
@@ -0,0 +1,13 @@
+#primero un while
+
+i = 10
+while i>0:
+    print(i)
+    i = i - 1
+	
+	
+#ahora aun for
+
+regiones = ["Arica", "Tarapaca", "Antofagasta", "Atacama"]
+for r in regiones:
+    print(r)
